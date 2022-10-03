ce31541097dcef88d5d3f3fc40182b3496951310
Tomas Thor Jonsson <>
2022-10-03 15:46:35 +0200

    Generate Accounts with User

--- lib/my_app_web.ex
+++ lib/my_app_web.ex
@@ -89,6 +89,7 @@ defmodule MyAppWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import MyAppWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
